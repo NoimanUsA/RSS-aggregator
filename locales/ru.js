05af6ce7e28f30bdd4c98ea006a9eda54c88173b
Noiman <noiman762@gmail.com>
2021-04-07 23:09:59 +0300

    changes locales

---
+++ locales/ru.js
@@ -0,0 +1,20 @@
+exports.ru = {
+  translation: {
+    form: {
+      added: 'RSS успешно загружен',
+      has: 'RSS уже существует',
+      isntRss: 'Ресурс не содержит валидный RSS',
+      istnUrl: 'Ссылка должна быть валидным URL',
+    },
+    feed: {
+      feedHead: 'Фиды',
+      postsHead: 'Посты',
+      postButton: 'Просмотр',
+    },
+
+    modal: {
+      readMore: 'Читать полностью',
+      closeModal: 'Закрыть',
+    },
+  },
+};
