f5809aef996a85e6881813fde42fc5fbad367b3d
Noiman <noiman762@gmail.com>
2021-04-13 20:21:41 +0300

    form update

--- src/renderers/form.js
+++ src/renderers/form.js
@@ -2,9 +2,10 @@ export default (state, i18n) => {
   const { form } = state;
   const color = form.validationDescription === 'added' ? 'text-success' : 'text-danger';
   const descContainer = document.querySelector('#feedback');
-  descContainer.classList.remove('text-sucess', 'text-danger');
+  descContainer.classList.remove('text-success', 'text-danger');
+
   descContainer.classList.add(color);
-  descContainer.innerHTML = i18n.t(`form.${form.validationDescription}`);
+  descContainer.innerText = i18n.t(`form.${form.validationDescription}`);
 
   form.state = 'waiting';
 };
