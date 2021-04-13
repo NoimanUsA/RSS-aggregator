ab2ac6030c4fd4b10d90a1d1c6944d92011f230c
Noiman <noiman762@gmail.com>
2021-04-13 14:38:52 +0300

    change index.html

--- src/app.js
+++ src/app.js
@@ -84,8 +84,7 @@ export default () => {
       .then(() => {
         updatePosts.timer(watcher);
       })
-      .catch((error) => {
-        console.log(error);
+      .catch(() => {
         state.form.validationDescription = 'netError';
         watcher.form.state = 'render';
       });
